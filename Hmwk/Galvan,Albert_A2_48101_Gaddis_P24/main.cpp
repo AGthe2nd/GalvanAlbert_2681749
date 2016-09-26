b827f5bdac84c8e28d911a9cca014fd8cd9c1ef8
rcc <rcc>
2016-09-26 11:06:21 -0700

    Signed-off-by: rcc <rcc>

---
+++ Hmwk/Galvan,Albert_A2_48101_Gaddis_P24/main.cpp
@@ -0,0 +1,53 @@
+/* 
+ * File:   main.cpp
+ * Author: Albert Galvan
+ * Created on 9/26/16 10:51
+ * Purpose:  Solve Problem 24 of Gaddis Chapter 3 8th Edition
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    string name, ctynam, cllgnam, profnam, animnam, petnam; 
+    int age;
+    
+    
+    //Input or initialize values Here
+    cout<<"Hello! \n";
+    cout<<"If you tell me the name of stuff, I'll tell you a story. Can you \n";
+    cout<<"tell me your name? \n";
+    cin>>name;
+    cout<<"Hello "<<name<<"! \n";
+    cout<<"What about your age? \n";
+    cin>>age;
+    cout<<"Now the name of a city"<<endl;
+    cin>>ctynam;
+    cout<<"Maybe the name of a college? \n";
+    cin>>cllgnam;
+    cout<<"How about a profession? \n";
+    cin>>profnam;
+    cout<<"Can you tell me the name of a animal? \n";
+    cin>>animnam;
+    cout<<"Why don't you give the animal a name? \n";
+    cin>>petnam;
+   
+    //Process/Calculations Here
+    
+    //Output Located Here
+    
+
+    //Exit
+    return 0;
+}
+
