c1d46b8c56d5e443800b74d9a10a908838fffa03
rcc <rcc>
2016-09-28 15:28:24 -0700

    Signed-off-by: rcc <rcc>

---
+++ Hmwk/Assignment_2/Galvan,Albert_A2_48101_Gaddis_P9/main.cpp
@@ -0,0 +1,46 @@
+/* 
+ * File:   main.cpp
+ * Author: Albert Galvan
+ * Created on September 28, 2016 3.06 PM
+ * Purpose:  Calculate how many calories come with cookies eaten.
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    
+    float cookate, calorie, maxcook, mincook;
+    
+    //Input or initialize values Here
+    
+    cout<<"Want to know how many calories you'll get if you eat cookies?\n";
+    cout<<"Tell me how many cookies you've ate or want to eat from a bag of 30\n";
+    cout<<"and I'll calculate how many calories you'll eat.";
+    cout<<"For this program, I am going under the assumption that a \n";
+    cout<<"single serving holds 300 calories and 30 cookies is 10 servings. \n";
+    
+    maxcook=30;
+    mincook=0;
+    
+    cin>>cookate;
+    //Process/Calculations Here
+    
+    //Output Located Here
+    
+    cout<<calorie;
+    
+    //Exit
+    return 0;
+}
+
