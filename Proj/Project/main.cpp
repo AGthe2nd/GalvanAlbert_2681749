414c19b7b9c18d197f231f9d6a5ce5bed8e25266
rcc <rcc>
2016-10-19 09:40:16 -0700

    Signed-off-by: rcc <rcc>

---
+++ Proj/Project/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Albert Galvan
+ * Created on October 19, 2016 9:14 AM
+ * Purpose:  Project
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    int playval, dealval, othval1, othval2, othval3, othval4, givcard, speccard;
+    
+    //Input or initialize values Here
+    
+    //Process/Calculations Here
+    
+    //Output Located Here
+    
+    //Exit
+    return 0;
+}
+
