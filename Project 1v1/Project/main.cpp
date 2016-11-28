db2e1ca820e05223f62732e434fa30fd333c514b
rcc <rcc>
2016-11-28 10:20:07 -0800

    Signed-off-by: rcc <rcc>

---
+++ Project 1v1/Project/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Albert Galvan
+ * Created on November 28th, 2016 10:19 AM
+ * Purpose:  Project that plays Blackjack
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    
+    //Input or initialize values Here
+    
+    //Process/Calculations Here
+    
+    //Prompts user to play again
+    
+    //Output Located Here
+
+    //Exit
+    return 0;
+}
